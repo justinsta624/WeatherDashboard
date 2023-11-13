65218d6bdf967f303dd094128151b24c06171e33
justinsta624 <justinsta624@gmail.com>
2023-11-12 23:36:08 -0500

    fix the error in JS, develop CSS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-// Link for open weather map API: https://home.openweathermap.org/api_keys
+// Link for OpenWeatherMap API current weather: https://openweathermap.org/current
+// Link for OpenWeatherMap API weather forecast for 5 days: https://openweathermap.org/forecast5
+
 // How to use DOM document query selector "external source: https://www.w3schools.com/jsref/met_document_queryselector.asp"
 var CityTextEl = document.querySelector("#city-text");
 var SearchHistoryBtnEl = document.querySelector("#search-history-button");
@@ -11,9 +13,16 @@ var FutureWeatherCondition = document.querySelector("#future-weather-condition")
 // Assigning an empty array
 var PastSearch = [];  
 
+SubmitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    var CityName = CityTextEl.value;
+    console.log(CityName);
+    OpenWeather(CityName);
+});
+
 function OpenWeather(CityName) {
-    // One call weather data: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-    // search by city name: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    // One call weather data: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}
+    // search by city name: https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}
     var APIKey = "3f1c7050fcbbe8196d43f120c87753f7";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + CityName + "&units=metric&appid=" + APIKey;
     // fetching weather data from the OpenWeatherMap API (search by city name)
@@ -33,8 +42,9 @@ function OpenWeather(CityName) {
                 // use JSON.stringify to Storing data in local storage
                 // external source: https://www.w3schools.com/js/js_json_stringify.asp
                 .then(function (OpenFutureWeather) {
-                    if (PastSearch.includes(OpenCurrentWeather.name) === false) {
+                    if (PastSearch.includes(OpenCurrentWeather.name) === false||PastSearch.includes("Reset") === false) {
                         PastSearch.push(OpenCurrentWeather.name);
+                        console.log(PastSearch);
                         localStorage.setItem("city", JSON.stringify(PastSearch));
                     }
                     OpenCity();
@@ -50,25 +60,25 @@ function OpenWeather(CityName) {
                     ${moment(OpenCurrentWeather.dt,"X").format("MM/DD/YYYY")}
                     </span></li> 
                     <li><img src ="http://openweathermap.org/img/wn/${OpenCurrentWeather.weather[0].icon}@2x.png" /></li> 
-                    <li>Temp: ${OpenCurrentWeather.main.temp}</li>
-                    <li>Wind: ${OpenCurrentWeather.wind.speed}</li>
-                    <li>Humidity: ${OpenCurrentWeather.main.humidity}</li>
+                    <li>Temperature: ${OpenCurrentWeather.main.temp}°C </li>
+                    <li>Humidity: ${OpenCurrentWeather.main.humidity}% </li>
+                    <li>Wind-Speed: ${OpenCurrentWeather.wind.speed}M/S </li>
                     </ul>`;
                     // From the OpenWeatherMap API;
                     // OpenCurrentWeather.name: name of the city
                     // ${moment(OpenCurrentWeather.dt,"X").format("MM/DD/YYYY")}: Uses the moment.js library to format the timestamp into a readable date format. 
                     // external source: https://momentjscom.readthedocs.io/en/latest/moment/01-parsing/03-string-format/
                     // Weather image ICON URL "external source: https://openweathermap.org/weather-conditions"
-                    // Display temperature, wind speed, humidity              
+                    // Display temperature, humidity, wind speed               
 
                     var WeatherCards = "";
                     for (var i = 1; i <= 5; i++) {
                     WeatherCards = WeatherCards + `<ul class="col-12 col-xl-2 day">
                     <li>${moment(OpenFutureWeather.daily[i].dt,"X").format(" MM/DD/YYYY")}</li>
                     <li><img src ="http://openweathermap.org/img/wn/${OpenFutureWeather.daily[i].weather[0].icon}@2x.png" /></li>
-                    <li>Temp: ${OpenFutureWeather.daily[i].temp.day}</li>
-                    <li>Wind: ${OpenFutureWeather.daily[i].wind_speed}</li>
-                    <li>Humidity: ${OpenFutureWeather.daily[i].humidity}</li>
+                    <li>Temperature: ${OpenFutureWeather.daily[i].temp.day}°C </li>
+                    <li>Wind-Speed: ${OpenFutureWeather.daily[i].wind_speed}M/S </li>
+                    <li>Humidity: ${OpenFutureWeather.daily[i].humidity}% </li>
                     </ul>`;
                     // From the OpenWeatherMap API;
                     // ${moment(OpenFutureWeather.daily[i].dt,"X").format(" MM/DD/YYYY")}: Uses the moment.js library to format the timestamp for the specific day into a readable date format.
@@ -89,11 +99,14 @@ function OpenCity() {
     // load saved data from the localStorage underneath the reset button
     var SearchList = "";
     for (var i = 0; i < PastSearch.length; i++) {
-        SearchList = SearchList + `<button class="btn btn-secondary my-2" type="submit">${PastSearch[i]}</button>`;
+        if (PastSearch[i] !== "Reset") {
+        SearchList = SearchList + `<button class="btn btn-secondary my-2 SearchBtn" type="submit">${PastSearch[i]}</button>`;
+        }
+        console.log(SearchList)
     }
     // innerHTML: replaces the content inside HTML element "#search-history-button" with variable function SearchList
     SearchHistoryBtnEl.innerHTML = SearchList;
-    var ShowHistory = document.querySelectorAll(".my-2");
+    var ShowHistory = document.querySelectorAll(".SearchBtn");
     for (var i = 0; i < ShowHistory.length; i++) {
         ShowHistory[i].addEventListener("click", function () {
             OpenWeather(this.textContent);
@@ -101,9 +114,6 @@ function OpenCity() {
     }
 }
 OpenCity();
-
-// Call the function WeatherDashboard when the SearchForm is submitted.
-SearchForm.addEventListener("submit", WeatherDashboard); 
 
 // Clear all items in localStorage & reset the PastSearch array
 // innerHTML: Clears the content inside the HTML element "#search-history-button"
